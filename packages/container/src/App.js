97060a0886ec780c02340cc9dc81853f34b51c9f
shrinivaswifidabba <shrinivas@wifidabba.com>
2024-05-11 12:24:33 +0530

    added random prefixes for production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,24 @@
+import {
+    StylesProvider,
+    createGenrateClassName
+} from '@material-ui/core/styles'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header'
 import MarketingApp from './components/MarketingApp'
 
+const generateClassName = createGenrateClassName({
+    productionPrefix: "co"
+})
 
 const App = () => {
     return (
-        <BrowserRouter>
-            <Header />
-            <MarketingApp />
-        </BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <Header />
+                <MarketingApp />
+            </BrowserRouter>
+        </StylesProvider>
     )
 }
 
